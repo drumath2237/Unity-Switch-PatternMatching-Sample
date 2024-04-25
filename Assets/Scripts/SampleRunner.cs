cc485b04e00b78e8309baedce574f933ed06b0cf
drumath2237 <2237k.drumath@gmail.com>
2024-04-25 15:41:01 +0900

    :sparkles: サンプルコードを実行する環境を整える

---
+++ Assets/Scripts/SampleRunner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityCSharpAwesomeSyntax.Samples;
+using UnityEngine;
+
+namespace UnityCSharpAwesomeSyntax
+{
+    public interface ISample
+    {
+        Task RunAsync(CancellationToken token);
+    }
+
+    public class SampleRunner : MonoBehaviour
+    {
+        private CancellationTokenSource _tokenSource;
+
+        private async Task Start()
+        {
+            _tokenSource = new CancellationTokenSource();
+
+            ISample sample = new SwitchAndPatternMatchingSample();
+
+            await sample.RunAsync(_tokenSource.Token);
+        }
+
+        private void OnDestroy()
+        {
+            _tokenSource.Cancel();
+            _tokenSource.Dispose();
+        }
+    }
+}
---
+++ Assets/Scripts/Samples/SwitchAndPatternMatchingSample.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityCSharpAwesomeSyntax.Samples
+{
+    public class SwitchAndPatternMatchingSample : ISample
+    {
+        public Task RunAsync(CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+            {
+                return Task.CompletedTask;
+            }
+
+            Debug.Log("Hello");
+            return Task.CompletedTask;
+        }
+    }
+}
